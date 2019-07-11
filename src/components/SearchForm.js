fb74dd5745463887937bd1fbccd7abdc00b246fa
Adam <apalm112@gmail.com>
2019-07-11 10:28:08 -0400

    Added autofocus to search input, added CSS styles to page.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -23,6 +23,7 @@ export default class SearchForm extends Component {
 						// ref is used to access the value of the input fields or the DOM element.  When used on an HTML element, the ref attribute takes a callback function that receives the underlying DOM element as its argument, in this case the input.
 						ref={(input) => this.query = input}
 						placeholder="Search Flickr photos"
+						autoFocus="autofocus"
 					/>
 					<button type="submit" className="search-button">
 						<svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
